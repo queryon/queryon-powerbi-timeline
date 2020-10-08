a8e77d5313ba54ee50301f821729b4674ef12b32
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-10-07 18:17:15 -0700

    Adds typed settings and methods to merge settings defaults with defined

--- src/settings.ts
+++ src/settings.ts
@@ -1,53 +1,125 @@
-// // /*
-// //  *  Power BI Visualizations
-// //  *
-// //  *  Copyright (c) Microsoft Corporation
-// //  *  All rights reserved.
-// //  *  MIT License
-// //  *
-// //  *  Permission is hereby granted, free of charge, to any person obtaining a copy
-// //  *  of this software and associated documentation files (the ""Software""), to deal
-// //  *  in the Software without restriction, including without limitation the rights
-// //  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// //  *  copies of the Software, and to permit persons to whom the Software is
-// //  *  furnished to do so, subject to the following conditions:
-// //  *
-// //  *  The above copyright notice and this permission notice shall be included in
-// //  *  all copies or substantial portions of the Software.
-// //  *
-// //  *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// //  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// //  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// //  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// //  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// //  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// //  *  THE SOFTWARE.
-// //  */
+import powerbi from "powerbi-visuals-api";
 
-// // "use strict";
 
-// import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
-// import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+export class Settings {
+    public download: DownloadSettings;
+    public textSettings: TextSettings;
+    public axisSettings: AxisSettings;
+    public style: StyleSettings;
+    public imageSettings: ImageSettings;
 
-// export class VisualSettings extends DataViewObjectsParser {
-// //       public textSettings: textSettingsSettings = new textSettingsSettings();
-// //       public dataPoint: dataPointSettings = new dataPointSettings()
-//       }
+    /** Constructs the default settings, and fills in and explicitly defined settings
+     * @param dataObjects the data objects, or undefined if no data objects exist
+     */
+    public constructor(dataObjects?: powerbi.DataViewObjects) {
+        this.download = DownloadSettings.FromViewObjects(dataObjects);
+        this.textSettings = TextSettings.FromViewObjects(dataObjects);
+        this.axisSettings = AxisSettings.FromViewObjects(dataObjects);
+        this.style = StyleSettings.FromViewObjects(dataObjects);
+        this.imageSettings = ImageSettings.FromViewObjects(dataObjects);
+    }
+}
 
-// //     export class textSettingsSettings {
-// //      // Default colora
-// //       public stagger: boolean = false
-// //      // Text Size
-// //       public fontSize: number = 12;
+/** Base class for all settings */
+export class SettingBase {
 
-// //       public fontFamily: string = "Arial"
-      
-// //      }
+    /** Generates this setting object from the provided view objects. Typically called from child class, not external callers */
+    public static GenerateSettingsObj<TSetting extends SettingBase>(type: (new () => TSetting), objects?: powerbi.DataViewObjects, ) : TSetting {
+        const settingName = 'download';
+        const settingsInstance = new type();
 
-// //      export class dataPointSettings {
-// //       // Default colora
-// //        public customFormat: boolean = false
-     
-// //       }
- 
- 
+        // Objects[settingsName] keys corresponds the property values on the settings classes
+        // Only continue if it is defined
+        if(objects && objects[settingName]) {
+
+            // Enumerate all properties, and check them against the DataViewObject keys
+            for(let key in settingsInstance) {
+                if(objects[settingName][key] !== undefined) {
+                    settingsInstance[key] = objects[settingName][key] as any; // as any workaround for lack of type checking here
+                }
+            }
+        }
+        return settingsInstance;
+    }
+}
+
+export class DownloadSettings extends SettingBase {
+    public downloadCalendar: boolean = false;
+    public position: string = 'TOP,LEFT';
+    public calendarName: string = '';
+
+    public static FromViewObjects(objects?: powerbi.DataViewObjects) : DownloadSettings {
+        return SettingBase.GenerateSettingsObj(DownloadSettings, objects);
+    }
+}
+
+export class TextSettings extends SettingBase {
+    public stagger: boolean = true;
+    public autoStagger: boolean =  true;
+    public spacing: boolean =  false;
+    public separator: string = ":";
+    public boldTitles: boolean =  false;
+    public annotationStyle: string =  "annotationLabel";
+    public labelOrientation: string =  "Auto";
+    public fontFamily: string =  "Arial";
+    public textSize: number = 12;
+    public textColor: powerbi.Fill = { solid: { color: 'Black' } };
+    public top: boolean =  false;
+    public dateFormat: string =  "same";
+    public customJS: string =  "MM/dd/yyyy";
+    public wrap: number = 400;
+
+    public static FromViewObjects(objects?: powerbi.DataViewObjects) : TextSettings {
+        return SettingBase.GenerateSettingsObj(TextSettings, objects);
+    }
+}
+
+export class AxisSettings extends SettingBase {
+    public axis: string = "None";
+    public dateFormat: string = "same";
+    public manualScale: boolean = false;
+    public manualScalePixel: boolean = false;
+    public axisColor: powerbi.Fill = { solid: { color: 'gray' } };
+    public fontSize: number = 12;
+    public fontFamily: string = 'Arial';
+    public bold: boolean = false;
+    public barMin: string = "";
+    public barMax: string = "";
+    public customPixel: string = "";
+    public customJS: string = "MM/dd/yyyy"
+
+    public static FromViewObjects(objects?: powerbi.DataViewObjects) : AxisSettings {
+        return SettingBase.GenerateSettingsObj(AxisSettings,  objects);
+    }
+}
+
+export class StyleSettings extends SettingBase {
+    public timelineStyle: string = "line";
+    public lineColor: powerbi.Fill = { solid: { color: 'black' } };
+    public lineThickness: number = 2;
+    public minimalistStyle: string = "circle";
+    public minimalistAxis: string = "bottom";
+    public iconsColor: powerbi.Fill = { solid: { color: 'black' } };
+    public minimalistConnect: boolean = false;
+    public connectColor: powerbi.Fill = { solid: { color: 'gray' } };
+    public minimalistSize: number = 2;
+    public barColor: powerbi.Fill = { solid: { color: 'rgb(186;215;57)' } };
+    public barHeight: number = 30;
+    public today: boolean = false;
+    public todayTop: boolean = true;
+    public todayColor: powerbi.Fill = { solid: { color: 'red' } }
+
+    public static FromViewObjects(objects?: powerbi.DataViewObjects) : StyleSettings {
+        return SettingBase.GenerateSettingsObj(StyleSettings, objects);
+    }
+}
+
+export class ImageSettings extends SettingBase {
+    public imagesHeight: number = 100;
+    public imagesWidth: number = 100;
+    public style: string = 'straight';
+
+    public static FromViewObjects(objects?: powerbi.DataViewObjects) : ImageSettings {
+        return SettingBase.GenerateSettingsObj(ImageSettings, objects);
+    }
+}
