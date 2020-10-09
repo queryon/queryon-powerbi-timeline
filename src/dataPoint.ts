cbcf62b82683ff599473f8752e953f848294a647
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-10-08 18:39:17 -0700

    Adds dataPoint type

---
+++ src/dataPoint.ts
@@ -0,0 +1,27 @@
+import powerbi from "powerbi-visuals-api";
+
+
+export class DataPoint {
+    public label: string;
+    public date: Date;
+    public URL: string | boolean;
+    public image: string | boolean;
+    public description: string;
+    public labelColumn: string;
+    public dateColumn: string;
+    public descriptionColumn: string | boolean;
+
+    public selectionId: powerbi.visuals.ISelectionId;
+    public dateAsInt: number;
+
+    public customFormat: boolean = false;
+    public fontFamily: string = "Arial";
+    public textSize: number = 12;
+    public textColor: string = 'black';
+    public iconColor: string = 'black';
+    public top: boolean = false;
+    public customVertical: boolean = false;
+    public verticalOffset: number = 20;
+    public annotationStyle: string = 'annotationLabel';
+    public labelOrientation: string = 'Auto';
+}
